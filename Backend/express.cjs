043f461d5b0e125ed8f8935bbae3a47ac3720a4e
SagiBeno <benosagi321@gmail.com>
2025-11-21 11:09:17 +0100

    Express, cors és mysql2 letöltése; /api/flowers endpoint létrehozása

---
+++ Backend/express.cjs
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nevenincs'
+});
+
+conn.connect(err => {
+    if (err) console.warn
+    else console.log('Succesfully connected to database: nevenincs.')
+});
+
+app.get('/api/flowers', (req, res) => {
+    conn.query(
+        `
+            SELECT aruk.id, aruk.nev, aruk.leiras, aruk.keszlet, aruk.ar, aruk.kepUrl, kategoriak.nev AS 'kategoria_nev'
+            FROM aruk
+            INNER JOIN kategoriak ON aruk.kategoriaId = kategoriak.id
+        `,
+        (err, result, fields) => {
+            if (err) res.status(500).json({ error: 'Database query error' });
+            if (result) res.status(200).json(result);
+        }
+    );
+});
+
+const port = 3333;
+app.listen(port, () => {
+    console.log('Express backend server is running on port:', port);
+});
+
