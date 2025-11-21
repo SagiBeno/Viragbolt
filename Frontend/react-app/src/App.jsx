a6df460011e3e139e6d059623a79ae6dc132894a
SagiBeno <benosagi321@gmail.com>
2025-11-21 14:21:57 +0100

    Kezdőoldal létrehozása

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './Components/HomePage'  
+//import Flowers from './Components/Flowers'
+//import Order from './Components/Order'
+import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        {/*<Route path='/flowers' element={<Flowers />} />
+        <Route path='/rendeles' element={<Order />} />*/}
+      </Routes>
     </>
   )
 }
